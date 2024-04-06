926582cc27e2746d9451260529c8c74cf6cfdf3e
Icegoo141 <isoonthipthanasup@gmail.com>
2024-04-06 23:26:42 +0700

    Setting Window

--- src/scene/MainMenuController.java
+++ src/scene/MainMenuController.java
@@ -1,4 +1,21 @@
 package scene;
 
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import utils.SceneNav;
+
+import java.io.IOException;
+
 public class MainMenuController {
+    @FXML
+    private Button setting;
+
+    @FXML
+    private void toSetting() {
+        SceneNav.setScene("SettingWindow");
+    }
 }
---
+++ src/scene/SettingWindowController.java
@@ -0,0 +1,43 @@
+package scene;
+
+import application.GameController;
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.text.Text;
+import utils.SceneNav;
+
+public class SettingWindowController {
+    private GameController gameInstance;
+    @FXML
+    private Slider soundSlider;
+
+    @FXML
+    private Text soundText;
+
+    @FXML
+    private Button toMenuButton;
+
+    public void initialize() {
+        gameInstance = GameController.getInstance();
+        int soundValue = gameInstance.getSoundValue();
+        soundSlider.setValue(soundValue);
+        soundText.setText(String.valueOf(soundValue));
+    }
+
+    @FXML
+    private void handleSoundSlider() {
+        int soundValue = (int) soundSlider.getValue();
+        soundText.setText(String.valueOf(soundValue));
+        gameInstance.setSoundValue(soundValue);
+    }
+
+    @FXML
+    private void backToMainMenu() {
+        SceneNav.setScene("MainMenu");
+    }
+}
