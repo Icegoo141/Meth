3db66c95302b9f750bdd54fb24ca5484f2d968dd
Icegoo141 <isoonthipthanasup@gmail.com>
2024-04-21 11:44:02 +0700

    add lives to HUD

--- src/application/logic/HUD.java
+++ src/application/logic/HUD.java
@@ -1,12 +1,15 @@
 package application.logic;
 
+import application.GameController;
 import application.sharedObject.IRenderable;
+import application.sharedObject.RenderableHolder;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class HUD implements IRenderable {
     private double remainingTime;
+
     @Override
     public int getZ() {
         return -9998;
@@ -15,11 +18,19 @@ public class HUD implements IRenderable {
     @Override
     public void draw(GraphicsContext gc) {
         gc.setFill(Color.GRAY);
-        gc.fillRect(800,0,200,800);
+        gc.fillRect(800, 0, 200, 800);
 
+        // Lives
+        int lives = GameController.getInstance().getGameLogic().getLives();
+        // Draw life images at specific coordinates based on the number of lives left
+        for (int i = 0; i < lives; i++) {
+            gc.drawImage(RenderableHolder.lifeSprite, 805 + i * 65, 100);
+        }
+
+        // Remaining Time
         gc.setStroke(Color.BLACK);
         gc.setFont(Font.font(30));
-        gc.strokeText(String.valueOf(Math.round(remainingTime * 10.0) / 10.0),850,200);
+        gc.strokeText(String.valueOf(Math.round(remainingTime * 10.0) / 10.0), 850, 200);
     }
 
     @Override
